06823d541f9a894732ab622c6db9e0705eca0985
yschindel <55532939+yschindel@users.noreply.github.com>
2022-11-23 16:50:48 +0100

    remove settings button

--- Revitron.tab/Qualitron.panel/Settings.pushbutton/Settings_script.py
+++
@@ -1,8 +0,0 @@
-import revitron
-from pyrevit import forms
-
-path = forms.save_file(file_ext='json', default_name='qualitronConfig')
-
-if path:
-    with revitron.Transaction():
-        revitron.DocumentConfigStorage().set('qualitron.configpath', path)
