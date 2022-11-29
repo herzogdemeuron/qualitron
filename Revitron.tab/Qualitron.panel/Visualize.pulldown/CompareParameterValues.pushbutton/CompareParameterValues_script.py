969c8f96a1828aa7d82671b8573f2572e55375dc
yschindel <55532939+yschindel@users.noreply.github.com>
2022-11-29 21:35:50 +0100

    add is on sheet test

--- Revitron.tab/Qualitron.panel/Visualize.pulldown/CompareParameterValues.pushbutton/CompareParameterValues_script.py
+++ Revitron.tab/Qualitron.panel/Visualize.pulldown/CompareParameterValues.pushbutton/CompareParameterValues_script.py
@@ -7,6 +7,10 @@ from revitron import _
 from qualitron import Parameter, Color
 from pyrevit import forms
 
+if _(revitron.ACTIVE_VIEW).get('Sheet Name'):
+    forms.alert('Current view is placed on sheet, choose a different view.', ok=True)
+    sys.exit()
+
 xamlFilesDir = op.dirname(__file__)
 xamlSource = 'ColorSwitchWindow.xaml'
 colorEqual = '#bfef45'
--- Revitron.tab/Qualitron.panel/Visualize.pulldown/Warnings.pushbutton/Warnings_script.py
+++ Revitron.tab/Qualitron.panel/Visualize.pulldown/Warnings.pushbutton/Warnings_script.py
@@ -1,5 +1,12 @@
 import revitron
 import qualitron
+import sys
+from revitron import _
+from pyrevit import forms
+
+if _(revitron.ACTIVE_VIEW).get('Sheet Name'):
+    forms.alert('Current view is placed on sheet, choose a different view.', ok=True)
+    sys.exit()
 
 with revitron.Transaction():
     qualitron.Warnings().colorElements()
